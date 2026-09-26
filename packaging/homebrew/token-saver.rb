9bfd3d610994f85696080b23b2eadb94998ab9b5
Ömer Faruk Eskitürk <omereskiturk2@gmail.com>
2026-09-26 15:08:35 +0300

    chore(release): sync package manifests with exact v1.0.1 binary SHA256 checksums

--- packaging/homebrew/token-saver.rb
+++ packaging/homebrew/token-saver.rb
@@ -7,16 +7,16 @@ class TokenSaver < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Farukes/Token-Saver/releases/download/v1.0.1/token-saver-darwin-arm64.tar.gz"
-      sha256 "9018a00efcaace0a32a8ebf95c313e0ed07ca6c6088ef38a3229c1bc06ab1801"
+      sha256 "01c5d96664be27704feb197619cdf47297dc4a9a6e9e941ddd0bc4a2a372bc07"
     else
       url "https://github.com/Farukes/Token-Saver/releases/download/v1.0.1/token-saver-darwin-x64.tar.gz"
-      sha256 "ecfe4c28fc859ea0ceec3c3a64ef107bff08701be8999f96056188d715facc18"
+      sha256 "74557a58d0a815cdaf828c3a46f2617280ba045aca6bdcb4f3160fb908dff845"
     end
   end
 
   on_linux do
     url "https://github.com/Farukes/Token-Saver/releases/download/v1.0.1/token-saver-linux-x64.tar.gz"
-    sha256 "4383e450623c1f01bf0ced3a27fa7344c7a7f0ec6694bac4ec09837014118e60"
+    sha256 "2b7c48a70fe0bb998e7928302c1d3eb9803226f62b597a34cc228d93edda6729"
   end
 
   def install
